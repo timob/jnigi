ec8a664b843b0807f313e11a8368c4248ec89e3d
Tim O'Brien <tim@tekao.net>
2016-06-23 19:23:54 +0100

    add simple benchmark

---
+++ bench_test.go
@@ -0,0 +1,23 @@
+package jnigi
+
+import (
+	"testing"
+)
+
+var obj *ObjectRef
+
+func BenchmarkSimple(b *testing.B) {
+	if obj == nil {
+		nenv := jvm.AttachCurrentThread()
+		var err error
+		obj, err = nenv.NewObject("java/lang/Integer", 0)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	b.ResetTimer()	
+	for i := 0; i < b.N; i++ {
+		obj.CallMethod(env, "intValue", Int)
+	}	
+}
