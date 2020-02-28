3ba46c228c0306743621a3f499e28e2b8fe4f8e4
Tim O'Brien <tim@tekao.net>
2020-02-28 13:59:02 +1100

    wip

--- compilevars.bat
+++ compilevars.bat
@@ -6,12 +6,10 @@ if x%jdkpath%==x goto fail
 
 set CGO_CFLAGS=-I%jdkpath%/include -I%jdkpath%/include/win32
 
-go install
-
 goto end
 
 :fail
 echo "jdk path not given" 
-echo "usage: ./install.bat <jdk path>"
+echo "usage: ./compilevars.bat <jdk path>"
 
 :end
